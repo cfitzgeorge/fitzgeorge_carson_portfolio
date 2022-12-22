d305ccd9b411b419661ed3ae0c7432f820338a1f
unknown <irealgoodcf@gmail.com>
2022-12-22 07:46:22 -0500

    final changes

---
+++ js/main.js
@@ -0,0 +1,30 @@
+import { SendMail } from "./components/mailer.js";
+
+(() => {
+    const { createApp } = Vue
+
+    createApp({
+        data() {
+            return {
+                message: 'Hello Vue!'
+            }
+        },
+
+        methods: {
+            processMailFailure(result) {
+           
+            },
+
+            processMailSuccess(result) {
+             
+            },
+
+            processMail(event) {        
+                // use the SendMail component to process mail
+                SendMail(this.$el.parentNode)
+                    .then(data => this.processMailSuccess(data))
+                    .catch(err => this.processMailFailure(err));
+            }
+        }
+    }).mount('#mail-form')
+})();
